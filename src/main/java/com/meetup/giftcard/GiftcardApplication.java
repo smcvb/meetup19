80c6ac81c7cece8f10c935b46c2e1c1262aa9c67
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-06-02 18:17:53 +0200

    Initial Commit - Spring Boot Starter

---
+++ src/main/java/com/meetup/giftcard/GiftcardApplication.java
@@ -0,0 +1,13 @@
+package com.meetup.giftcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftcardApplication.class, args);
+	}
+
+}
